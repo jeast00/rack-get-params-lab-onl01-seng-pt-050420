8765b8ee0008c90cf117a29c1644b4d4bd825b77
Jonathan East <jonathan_east@ymail.com>
2020-07-24 17:19:56 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -18,6 +18,9 @@ class Application
       if @@cart.empty?
         resp.write "Your cart is empty."
       else
+        @@cart.each do |item|
+          resp.write "#{item}"
+        end
     else
       resp.write "Path Not Found"
     end
